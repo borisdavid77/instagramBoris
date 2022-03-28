f157afa64aa25e9216b55a81d3f822ef76b49aa5
borisdavid77 <74749789+borisdavid77@users.noreply.github.com>
2022-03-28 10:17:05 -0500

    Delete sidebar.js

--- src/components/sidebar.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import useUser from "../hooks/use-user";
-
-export default function Sidebar() {
-  const { user } = useUser();
-  return <div>sidebar</div>;
-}
