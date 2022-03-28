8c2a6babdb54411a0ed3abe89faaea698458e719
borisdavid77 <borisdavid779@gmail.com>
2022-03-28 09:14:07 -0500

    finally

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 /* eslint-disable react/jsx-no-constructed-context-values */
 import "./wdyr";
-=======
->>>>>>> 7ae4ad4a182b28aa5d9d73ad9f3d040d6bc884b0
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
@@ -11,11 +8,7 @@ import { firebase, FieldValue } from "./lib/firebase";
 import "./styles/app.css";
 
 ReactDOM.render(
-<<<<<<< HEAD
   <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-=======
-  <FirebaseContext.Provider value={(firebase, { FieldValue })}>
->>>>>>> 7ae4ad4a182b28aa5d9d73ad9f3d040d6bc884b0
     <App />
   </FirebaseContext.Provider>,
   document.getElementById("root")
